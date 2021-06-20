af43e94a487d312fad36cbc2f62084932a91ede2
Gabriel Chiarelli Noble <gabrielcnoble@gmail.com>
2021-06-20 15:00:13 -0300

    revision 1

---
+++ gui.h
@@ -0,0 +1,16 @@
+#ifndef GUI_H
+#define GUI_H
+
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui/cimgui.h"
+
+void gui_Init();
+
+void gui_Shutdown();
+
+void gui_BeginFrame();
+
+void gui_EndFrame();
+
+
+#endif // GUI_H
